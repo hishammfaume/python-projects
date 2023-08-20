16c07fed32ad180d21a4d46c02c43cf145734e72
hishammfaume <hishammfaume@gmail.com>
2023-08-20 15:25:54 +0300

    this is a screenshot GUI

--- screenshot.py
+++ screenshot.py
@@ -1,11 +1,31 @@
 import time
 import pyautogui
+import tkinter as tk
 
+#function to take screenshot
 def screenshot():
-    name = int(round(time.time() * 10000))
-    name = '{}.png'.format(name)
-    time.sleep(4) 
-    img  = pyautogui.screenshot(name)
+    time.sleep(4) #four scecond for the screenshot function to run
+    name = int(round(time.time()* 1000))
+    name = 'C:/Users/hisha/python-projects/screenshots-folder.png'.format(name)
+    img = pyautogui.screenshot(name)
     img.show()
 
-screenshot()
+#creating the GUI for the screenshot function
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button (
+    frame,
+    text = "Take screenshot",
+    command = screenshot
+)
+button.pack(side=tk.LEFT)
+
+close = tk.Button (
+    frame,
+    text = "QUIT",
+    command=quit)
+close.pack(side=tk.RIGHT)
+
+root.mainloop()
